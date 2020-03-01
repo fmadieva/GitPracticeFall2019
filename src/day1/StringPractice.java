f718c72cc3a04dabd40e1aa652e15d17265d8a1c
fmadieva <fariza.madieva@gmail.com>
2020-03-01 12:39:06 -0600

    modified String practicce class

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -5,6 +5,9 @@ public class StringPractice {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("great!");
+        System.out.println("I have no idea whats happening! ");
+        String str = "Java is fun!";
+        System.out.println(str);
 
     }
 }
